8fd8ce3d29e1fad7644402666e792409a8c45366
T Prayag <prayagtushar2016@gmail.com>
2025-03-02 00:16:05 +0530

    changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import { requireAuth } from '@clerk/express';
 import chatRoutes from './routes/chat.routes';
 import uploadRoute from './routes/upload.routes';
-import errorHandler from './middleware/errorHandler';
 
 dotenv.config();
 const app = express();
@@ -17,8 +16,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', requireAuth(), uploadRoute);
 app.use('/api', requireAuth(), chatRoutes);
-
-app.use(errorHandler);
 
 app.listen(port, () => {
 	console.log(`Server running on http://localhost:${port}`);
